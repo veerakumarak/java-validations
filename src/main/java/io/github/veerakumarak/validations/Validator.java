d7946ff85f43177a0509f63e6d6ec8ed7e520572
veera kumar <veerakumarak@gmail.com>
2025-08-21 08:30:56 +0530

    added a helper method

--- src/main/java/io/github/veerakumarak/validations/Validator.java
+++ src/main/java/io/github/veerakumarak/validations/Validator.java
@@ -2,6 +2,8 @@ package io.github.veerakumarak.validations;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class Validator {
 
@@ -18,7 +20,15 @@ public final class Validator {
 		return allValid(List.of(fieldResults));
 	}
 
-	public static ValidationResult anyValid(List<FieldResult> fieldResults) {
+    public static ValidationResult allValid(List<FieldResult> first, List<FieldResult> second) {
+        return allValid(Stream.concat(first.stream(), second.stream()).toList());
+    }
+
+    public static ValidationResult allValid(FieldResult first, List<FieldResult> second) {
+        return allValid(List.of(first), second);
+    }
+
+    public static ValidationResult anyValid(List<FieldResult> fieldResults) {
 		Objects.requireNonNull(fieldResults, "Results list cannot be null.");
 		if (!fieldResults.isEmpty() && fieldResults.stream().anyMatch(FieldResult::valid)) {
 			return ValidationResult.of(List.of());
@@ -29,5 +39,14 @@ public final class Validator {
 	public static ValidationResult anyValid(FieldResult... fieldResults) {
 		return anyValid(List.of(fieldResults));
 	}
+
+    public static ValidationResult anyValid(List<FieldResult> first, List<FieldResult> second) {
+        return anyValid(Stream.concat(first.stream(), second.stream()).toList());
+    }
+
+    public static ValidationResult anyValid(FieldResult first, List<FieldResult> second) {
+        return anyValid(List.of(first), second);
+    }
+
 
 }
